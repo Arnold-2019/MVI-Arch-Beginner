b7ff0c1b95342356eeda374beb057f5b1096f94d
Arnold Wang <arnold.wang@rea-group.com>
2021-11-25 22:15:59 +0800

    test

--- app/src/main/java/com/example/mvi_arch_beginner/view/MainActivity.kt
+++ app/src/main/java/com/example/mvi_arch_beginner/view/MainActivity.kt
@@ -42,6 +42,7 @@ class MainActivity : AppCompatActivity() {
         setupClicks()
     }
 
+
     @ExperimentalCoroutinesApi
     private fun setupClicks() {
         buttonFetchUser.setOnClickListener {
